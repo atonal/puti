3054063518e1c1096c8748c4af38e7b06cbb007b
Tuukka Koistinen <tvkoisti@gmail.com>
2019-07-13 18:17:47 +0300

    putify: move route and rename html template

--- webapp/putify.py
+++ webapp/putify.py
@@ -2,7 +2,7 @@ import generate_blog
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/putify")
 def hello():
     title, paragraphs= generate_blog.generate_post()
-    return render_template('index.html', title=title, paragraphs=paragraphs)
+    return render_template('putify.html', title=title, paragraphs=paragraphs)
