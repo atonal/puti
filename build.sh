db29ea84c55b5c1f5317923e591316bb98388a79
Tuukka Koistinen <tvkoisti@gmail.com>
2021-01-09 19:11:33 +0200

    build: do multi platform build by default

--- build.sh
+++ build.sh
@@ -1,1 +1,20 @@
-docker build -t atonal/puti .
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+script_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+repo_root=$(cd "${script_dir}" && git rev-parse --show-toplevel)
+
+docker_tags=(
+    -t atonal/puti:dev
+)
+
+for git_tag in $(git tag --points-at HEAD); do
+    docker_tags+=("-t atonal/puti:${git_tag}")
+done
+
+# old build:
+# docker build -t atonal/puti .
+
+# multi-platform build:
+docker buildx build "${docker_tags[@]}" --platform linux/amd64,linux/arm/v7 --push "${repo_root}"
